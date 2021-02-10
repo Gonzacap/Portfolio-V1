ab1f4a8e8c4354c92b4863f250ffaafcc232bca7
Gonzacap <gonzalo.october@gmail.com>
2021-02-10 15:11:42 -0300

    project-cards with vue

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+Vue.component('project-card',{
+    props: ['img', 'title', 'description', 'app', 'code', 'btnStyle'],
+    /*html*/
+    template: `
+        <div class="card p-1 m-2">
+
+            <img :src="img" alt="imagen de la targeta" class="card-img-top img-fluid p-2">
+            <h5 class="card-title">{{title}}</h5>
+            <p class="card-text text-justify p-2">
+                {{description}}
+            </p>
+            <a :href="app" class="btn btn-primary btn-md m-1" target="_blank" :style="btnStyle">App</a>
+            <a :href="code" class="btn btn-secondary btn-md m-1"  target="_blank">Codigo</a>
+    
+        </div>
+    `,
+});
+
 Vue.component('it-card-1',{
     props: ['img', 'title'],
     /*html*/
@@ -42,8 +60,81 @@ const app = new Vue({
             {title:"Framework: Node.js", ico:"fab fa-node-js fa-8x"},
             {title:"CMS: WordPress", ico:"fab fa-wordpress fa-8x"},
             {title:"Control de Versiones: Git", ico:"fab fa-git-alt fa-8x"}
-        ]
-
+        ],
+        projects:[
+            {
+                img:"img/img-01.jpg",
+                title:"My Link Tree",
+                description:"Mi propio link tree hecho con HTML y CSS.", 
+                app:"https://gonzacap.github.io/My-Link-Tree/",
+                code:"https://github.com/Gonzacap/My-Link-Tree",
+                btnStyle:" "
+            },
+            {
+                "img":"img/img-02.jpg",
+                "title":"CRUD Vue",
+                "description":"CRUD con Vue.js, Bootstrap y LocalStorage; realizado como practica de Vue.", 
+                "app":"https://gonzacap.github.io/CRUD-LocalStorage-con-Vue/",
+                "code":"https://github.com/Gonzacap/CRUD-LocalStorage-con-Vue",
+                "btnStyle":"background-color: red;"
+            },
+            {
+                "img":"img/img-03.png",
+                "title":"Snake v1.0",
+                "description":"Un dia estaba aburrido, era de madrugada y se me dio por hacer el juego de la vivorita para practicar JavaScript.", 
+                "app":"https://gonzacap.github.io/Snake-v1.0/",
+                "code":"https://github.com/Gonzacap/Snake-v1.0",
+                "btnStyle":"background-color: red;"
+            },
+            {
+                "img":"img/img-04.jpg",
+                "title":"Pagina Tributo",
+                "description":"Página tributo realizada como proyecto de Web Design de FreeCodeCamp. PD: Soy fan de Magnus Mefsto.", 
+                "app":"https://gonzacap.github.io/Pagina-Tributo/",
+                "code":"https://github.com/Gonzacap/Pagina-Tributo",
+                "btnStyle":"background-color: red;"
+            },
+            {
+                "img":"img/img-05.jpg",
+                "title":"Reloj JS",
+                "description":"Reloj responsive hecho como Practica de JavaScript.", 
+                "app":"https://gonzacap.github.io/Reloj-en-JavaScript/",
+                "code":"https://github.com/Gonzacap/Reloj-en-JavaScript",
+                "btnStyle":"background-color: red;"
+            },
+            {
+                "img":"img/img-06.jpg",
+                "title":"Login HTML + CSS",
+                "description":"Login hecho como practica de maquetado en HTML y CSS.", 
+                "app":"https://gonzacap.github.io/Login/",
+                "code":"https://github.com/Gonzacap/Login",
+                "btnStyle":"background-color: red;"
+            }/*,
+            {
+                "img":"img/img-07.jpg",
+                "title":"App Venta de Maquillaje",
+                "description":"Ejemplo de una app de venta de maquillajes realizada con Mat|r Script. Esta fue mi primera experiencia en el desarrollo movil.", 
+                "app":"#",
+                "code":"https://github.com/Gonzacap/ejemplo-app-venta-de-maquillaje",
+                "btnStyle":"pointer-events: none;"
+            },
+            {
+                "img":"img/img-08.png",
+                "title":"Buddy System Algorithm",
+                "description":"Simulacion del funcionamiento del algorimo Buddy (Algoritmo de Sistema Compañero) para asignación de memoria. Realizado en C.", 
+                "app":"#",
+                "code":"https://github.com/Gonzacap/Buddy-System-Algoritm---Algoritmo-de-Sistema-Compa-ero",
+                "btnStyle":"pointer-events: none;"
+            },
+            {
+                "img":"img/img-09.png",
+                "title":"Sistema de Gestion Logistica",
+                "description":"ABM de camiones, plantas, rutas e insumos; realizado en Java, Java Swing y MySQL para la materia de DIED (Diseño e Implementacion de Esctructuras de Datos).", 
+                "app":"#",
+                "code":"https://github.com/Gonzacap/gestionLogistica",
+                "btnStyle":"pointer-events: none;"
+            }*/
+        ],
     },
     /*html*/
 });
